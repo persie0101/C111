d1478bb2330eecf57dc5815b4d1769458b11ddb5
persie0101 <125737725+persie0101@users.noreply.github.com>
2023-04-09 12:04:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -21,3 +21,17 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded(){
     console.log("modelLoaded")
 }
+function check(){
+    img=document.getElementById('captured_image')
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+} 
